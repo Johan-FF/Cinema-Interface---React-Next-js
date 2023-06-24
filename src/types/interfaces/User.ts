57b0d4d3b76ef0e2378a8a8ccbedc757d189a489
Johan FF <johan.artmat@gmail.com>
2023-06-24 04:34:26 -0500

    Bug de atributos en interfaz NewUser

--- src/types/interfaces/User.ts
+++ src/types/interfaces/User.ts
@@ -1,6 +1,10 @@
 import Dato from "./Dato"
 
 export interface NewUser extends Dato {
+  name: string,
+  position: string,
+  phoneNumber: string,
+  identification: string,
   password: string,
   conPassword: string,
 }
