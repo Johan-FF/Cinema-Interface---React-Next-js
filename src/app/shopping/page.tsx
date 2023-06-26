32845a5f03df7d5ccbbce62a8a7980707ffa90ea
JohanVillabona <villabona.johan@gmail.com>
2023-06-25 22:59:10 -0500

    implementacion creacion y busqueda de empleado

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,47 +1,108 @@
-'use client'
-import { useState } from 'react'
-import { products } from '@/app/types/data/ProductData'
-import CardList from '@/app/components/CardList'
-import Search from '../../components/Search'
-import NavBar from '@/app/components/NavBar'
+"use client";
+import { useState } from "react";
+import { products } from "@/app/types/data/ProductData";
+import CardList from "@/app/components/CardList";
+import Search from "../../components/Search";
+import NavBar from "@/app/components/NavBar";
+import AddEmployer from "../employee-management/addEmployer";
+import EmployeeManagement from "../employee-management/page";
 
 export default function Shopping() {
-  const [productType, setProductType] = useState('movies')
-  const [searchTerm, setSearchTerm] = useState('')
+  const [productType, setProductType] = useState("movies");
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value)
-  }
+    setSearchTerm(event.target.value);
+  };
   const changeTypeProduct = (type: string) => {
-    setProductType(type)
-  }
+    setProductType(type);
+  };
   const snacksData = products.snacks.filter((item) => {
     return (
       item.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
       item.productPrice.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  })
+    );
+  });
   const moviesData = products.movies.filter((item) => {
     return (
       item.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
       item.productPrice.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  })
+    );
+  });
 
   return (
     <main className="h-[100vh] w-[100%] relative">
-        <NavBar changeProduct={changeTypeProduct} />
-        {
-          productType === 'movies' ?
-            <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
-              <Search title='Películas' searchTerm={searchTerm} handleSearchChange={handleSearchChange}/>
-              <CardList typeProduct='movie' filteredData={moviesData}/>
-            </section>
-          :
-            <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
-              <Search title='Comidas' searchTerm={searchTerm} handleSearchChange={handleSearchChange}/>
-              <CardList typeProduct='snack' filteredData={snacksData}/>
-            </section>
+      <NavBar changeProduct={changeTypeProduct} />
+
+      {(() => {
+        switch (productType) {
+          case "movies":
+            return (
+              <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+                <Search
+                  title="Películas"
+                  searchTerm={searchTerm}
+                  handleSearchChange={handleSearchChange}
+                />
+                <CardList typeProduct="movie" filteredData={moviesData} />
+              </section>
+            );
+          case "snack":
+            return (
+              //No se que paso pero ya muestra el titulo y la barra de busqueda
+              <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+                <Search
+                  title="Comidas"
+                  searchTerm={searchTerm}
+                  handleSearchChange={handleSearchChange}
+                />
+                <CardList typeProduct="snack" filteredData={snacksData} />
+              </section>
+            );
+          case "employee":
+            return (
+              <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+                <Search
+                  title="Registro empleados"
+                  searchTerm={searchTerm}
+                  handleSearchChange={handleSearchChange}
+                />
+                <AddEmployer typeProduct="employee" />
+              </section>
+            );
+          case "searchemployee":
+            return (
+              <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+                <Search
+                  title="Busqueda empleados"
+                  searchTerm={searchTerm}
+                  handleSearchChange={handleSearchChange}
+                />
+                <EmployeeManagement typeProduct="searchemployee" />
+              </section>
+            );
+          default:
+            return <CardList typeProduct="snack" filteredData={snacksData} />;
         }
+      })()}
+      {/* {productType === "movies" ? (
+        <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+          <Search
+            title="Películas"
+            searchTerm={searchTerm}
+            handleSearchChange={handleSearchChange}
+          />
+          <CardList typeProduct="movie" filteredData={moviesData} />
+        </section>
+      ) : (
+        <section className="z-1 h-[100%] w-[100%] absolute md:left-[20%] md:w-[80%] pb-[5%] bg-primary ">
+          <Search
+            title="Comidas"
+            searchTerm={searchTerm}
+            handleSearchChange={handleSearchChange}
+          />
+          <CardList typeProduct="snack" filteredData={snacksData} />
+        </section>
+      )} */}
     </main>
-  )
+  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,113 @@
-import Image from "next/image"
-import BlueButton from "./BlueButton"
-import props from "../types/props/NavBarProps"
-import { useState } from "react"
+import Image from "next/image";
+import BlueButton from "./BlueButton";
+import props from "../types/props/NavBarProps";
+import { useState } from "react";
 
 export default function NavBar({ changeProduct }: props) {
-  const [viewMenu, setViewMenu] = useState(false)
+  const [viewMenu, setViewMenu] = useState(false);
 
   return (
-    <nav className={`z-50 absolute left-0 top-0 transition-all duration-500 h-[100%] ${viewMenu?'w-[100%]':' w-0 md:w-[20%]'} bg-tertiary shadow-2xl shadow-black`}> 
-      <section className={`${viewMenu?'visible':'invisible md:visible'} w-[100%] h-[40%] flex flex-col justify-center items-center`}>
-        <Image className="shadow-2xl shadow-black" src="/iconoPanelControl.webp" alt="Icono Cinema" width={500} height={500}/>
-        <h2 className="text-[20px] w-full text-center py-0 my-0 border-t-2 border-primary pt-2 rounded-lg">Carmen de los Ríos</h2>
-        <h3 className="text-[15px] w-full text-center py-0 my-0 text-opacity-90 border-b-2 border-primary pb-2 rounded-lg shadow-2xl shadow-black">Gerente</h3>
+    <nav
+      className={`z-50 absolute left-0 top-0 transition-all duration-500 h-[120%] ${
+        viewMenu ? "w-[100%]" : " w-0 md:w-[20%]"
+      } bg-tertiary shadow-2xl shadow-black`}
+    >
+      <section
+        className={`${
+          viewMenu ? "visible" : "invisible md:visible"
+        } w-[100%] h-[40%] flex flex-col justify-center items-center`}
+      >
+        <Image
+          className="shadow-2xl shadow-black"
+          src="/iconoPanelControl.webp"
+          alt="Icono Cinema"
+          width={500}
+          height={500}
+        />
+        <h2 className="text-[20px] w-full text-center py-0 my-0 border-t-2 border-primary pt-2 rounded-lg">
+          Carmen de los Ríos
+        </h2>
+        <h3 className="text-[15px] w-full text-center py-0 my-0 text-opacity-90 border-b-2 border-primary pb-2 rounded-lg shadow-2xl shadow-black">
+          Gerente
+        </h3>
       </section>
-      <ul className={`${viewMenu?'visible':'invisible md:visible'} w-[100%] h-[40%] flex flex-col justify-center items-center`}>
-        <li onClick={() => {changeProduct('snacks')}} className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black">Comida</li>
-        <li onClick={() => {changeProduct('movies')}} className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black">Películas</li>
+      <ul
+        className={`${
+          viewMenu ? "visible" : "invisible md:visible"
+        } w-[100%] h-[40%] flex flex-col justify-center items-center`}
+      >
+        <li
+          onClick={() => {
+            changeProduct("snacks");
+          }}
+          className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black"
+        >
+          Comida
+        </li>
+        <li
+          onClick={() => {
+            changeProduct("movies");
+          }}
+          className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black"
+        >
+          Películas
+        </li>
+        <li
+          onClick={() => {
+            changeProduct("employee");
+          }}
+          className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black"
+        >
+          Ingreso Empleados
+        </li>
+        <li
+          onClick={() => {
+            changeProduct("searchemployee");
+          }}
+          className="h-[20%] w-[100%] border-y-2 my-1 border-primary hover:bg-tertiary-opacity flex justify-center items-center cursor-pointer shadow-2xl shadow-black"
+        >
+          Busqueda Empleados
+        </li>
       </ul>
-      <section className={`${viewMenu?'visible':'invisible md:visible'} w-[100%] h-[20%] flex justify-center items-center`}>
+      <section
+        className={`${
+          viewMenu ? "visible" : "invisible md:visible"
+        } w-[100%] h-[20%] flex justify-center items-center`}
+      >
         <div className="h-[30%]">
-          <BlueButton content="Cerrar sesión" leftRounded={true} rightRounded={true}/>
+          <BlueButton
+            content="Cerrar sesión"
+            leftRounded={true}
+            rightRounded={true}
+          />
         </div>
       </section>
-      <button onClick={() => {setViewMenu(!viewMenu)}} className={`z-100 md:hidden transition-all duration-1000 absolute top-[1%] left-[15%] h-[5vh] w-[5vh] bg-tertiary hover:bg-tertiary-opacity rounded-2xl ${viewMenu?'rotate-180':''}`}>
+      <button
+        onClick={() => {
+          setViewMenu(!viewMenu);
+        }}
+        className={`z-100 md:hidden transition-all duration-1000 absolute top-[1%] left-[15%] h-[5vh] w-[5vh] bg-tertiary hover:bg-tertiary-opacity rounded-2xl ${
+          viewMenu ? "rotate-180" : ""
+        }`}
+      >
         <div className="h-full w-full relative rounded-2xl">
-          <span className={`transition-all duration-500 w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] ${viewMenu?' rotate-45 translate-y-2 top-[22%] ':'top-[18%]'}`}></span>
-          <span className={`w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] top-[48%] ${viewMenu?'hidden':''}`}></span>
-          <span className={`transition-all duration-500 w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] ${viewMenu?'-rotate-45 -translate-y-2 top-[72%] ':'top-[78%]'}`}></span>
+          <span
+            className={`transition-all duration-500 w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] ${
+              viewMenu ? " rotate-45 translate-y-2 top-[22%] " : "top-[18%]"
+            }`}
+          ></span>
+          <span
+            className={`w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] top-[48%] ${
+              viewMenu ? "hidden" : ""
+            }`}
+          ></span>
+          <span
+            className={`transition-all duration-500 w-[70%] h-[8%] rounded-lg bg-primary absolute left-[15%] ${
+              viewMenu ? "-rotate-45 -translate-y-2 top-[72%] " : "top-[78%]"
+            }`}
+          ></span>
         </div>
       </button>
     </nav>
-  )
+  );
 }
