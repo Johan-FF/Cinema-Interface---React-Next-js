92b29c82305378d3eb702063d40b5a7aaa6dd17b
Johan FF <johan.artmat@gmail.com>
2023-07-07 14:05:30 -0500

    bug auth service

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,14 +1,12 @@
 import { post } from "./HttpService"
 import Account from "./Account"
 import { API_URL, KEY_USER_TOKEN } from "../environment"
-import { cookies } from "next/dist/client/components/headers"
 
 export async function login(userData: any): Promise<any> {
   const url = `${API_URL}/auth/login`
   const response: Promise<any> =  post(url, userData)
   response
   .then(responseFetch => {
-      cookies().set(KEY_USER_TOKEN, responseFetch.token)
       const account: Account = Account.getInstance()
       account.setName(responseFetch.name)
       account.setRol(responseFetch.rol)
